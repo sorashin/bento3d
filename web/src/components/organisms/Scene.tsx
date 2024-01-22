ba446061aa1787c76c5e1b62bd0e82707c85125e
sorashin <naka52shin@gmail.com>
2024-01-23 04:55:19 +0900

    move nuxt folder

--- web/src/components/organisms/Scene.tsx
+++ web/src/components/organisms/Scene.tsx
@@ -1,41 +1,89 @@
 
-import { Canvas } from '@react-three/fiber'
-import {   GizmoHelper, GizmoViewport, OrbitControls } from '@react-three/drei'
-import { Suspense, useEffect } from 'react';
-import * as Graph  from '@nodi/core';
+import { useThree, Canvas, useFrame } from '@react-three/fiber'
+import {   Edges, GizmoHelper, GizmoViewport, OrbitControls, Stage } from '@react-three/drei'
+import { Suspense, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { IElementable } from '@nodi/core';
+import * as THREE from 'three';
+import { JSX } from 'react/jsx-runtime';
+import { Bounds} from '@react-three/drei'
 
 
+const CanvasSetup: React.FC = () => {
+  const { camera } = useThree();
 
-function Scene() {
   useEffect(() => {
-    const graph = Graph;
-    console.log(graph)
-  }, [])
+    camera.up.set(0, 0, 1);
+  }, [camera]);
 
   return (
+    <>
+    <color attach="background" args={['#dadada']} />
+    {/* <primitive object={new THREE.AxesHelper(10)} /> */}
+    <GizmoHelper alignment="bottom-left" margin={[80, 80]} onUpdate={() => {}}>
+          <GizmoViewport axisColors={['#FD5B5D', '#38E2B3', '#2B99FF']} labelColor="#fff" />
+        </GizmoHelper>
+        <group rotation={[Math.PI/2,0,0]}>
+        <gridHelper args={[20, 20, 0xbbbbbb, 0xcccccc]} />
+        </group>
+        <OrbitControls enablePan={false} enableZoom={true} enableRotate={true} makeDefault dampingFactor={0.3} />
+    </>
+  )
+}
+
+const Content: React.FC<{ group: THREE.Group }> = ({ group }) => {
+  const contentRef = useRef<THREE.Group>(null);
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    const rotationSpeed = 0.2; // 回転速度
+
+    // Z軸周りに回転
+    if (contentRef.current) {
+      contentRef.current.rotation.z = elapsedTime * rotationSpeed;
+    }
+  });
+  return(
+    <group ref={contentRef}>
+            {group && (
+              //apply hotpink color to group
+              group.children.map((child, index) => {
+                return(
+              <primitive object={child} key={index} >
+                {/* <meshStandardMaterial transparent opacity={0.1}/> */}
+                <meshStandardMaterial color={new THREE.Color("rgb(210, 182, 196)")}/>
+                {/* set #333333 color to <Edge> */}
+
+                <Edges color={new THREE.Color("rgb(196, 196, 196)")}/>
+
+              </primitive>
+                )
+              })
+            )}
+    </group>
+  )
+}
+
+interface SceneProps {
+  group: THREE.Group;
+}
+
+export const Scene: React.FC<SceneProps> = ({ group }) => {
+  return (
     <div className='fixed inset-0'>
-      
-    <Canvas camera={{ fov: 50, position: [3, 3, 3] }}>
-      <color attach="background" args={['#dadada']} />
-      <Suspense fallback={null}>
-      <mesh
-        scale={1}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={'hotpink'} />
-      </mesh>
-        <ambientLight/>
-        <hemisphereLight intensity={0.4} groundColor="white" />
-        <directionalLight position={[10, 15, -10]} intensity={0.5} />
-        <spotLight position={[5, 10, -15]} intensity={1} angle={0.1} penumbra={1} castShadow shadow-mapSize={[1024, 1024]} shadow-bias={-0.000001} />
-      </Suspense>
-      {/* <primitive object={new THREE.AxesHelper(10)} /> */}
-      <GizmoHelper alignment="bottom-left" margin={[80, 80]} onUpdate={() => {}}>
-        <GizmoViewport axisColors={['#FD5B5D', '#38E2B3', '#2B99FF']} labelColor="#fff" />
-      </GizmoHelper>
-      <gridHelper args={[20, 20, 0xbbbbbb, 0xcccccc]} />
-      <OrbitControls enablePan={false} enableZoom={true} enableRotate={true} makeDefault dampingFactor={0.3}/>
-      {/* <ArcballControls  enablePan={false} makeDefault /> */}
-    </Canvas>
+      <Canvas camera={{ fov: 50, position: [300, 300, 300] }}>
+        <CanvasSetup/>
+        
+        <Suspense fallback={null}>
+          <Stage shadows={"contact"}>
+            <Content group={group}/>
+          </Stage>
+          <ambientLight />
+          <hemisphereLight intensity={0.4} groundColor="white" />
+          <directionalLight position={[10, 15, -10]} intensity={0.5} />
+          <spotLight position={[5, 10, -15]} intensity={1} angle={0.1} penumbra={1} castShadow shadow-mapSize={[1024, 1024]} shadow-bias={-0.000001} />
+        </Suspense>
+        
+        {/* <ArcballControls  enablePan={false} makeDefault /> */}
+      </Canvas>
     </div>
   )
 }
---
+++ web/src/firebase/firebase.ts
@@ -0,0 +1,74 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+import Project, { ProjectJSONType } from '../assets/scripts/service/Project';
+
+//TODO: replace your firebase config
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+
+if (!firebase.apps.length) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type GetProjectResponse = Promise<{ project: Project, doc: any }>;
+
+const graphCollectionName = 'graph';
+
+export const getProjectDocument = async (id: string): Promise<firebase.firestore.DocumentData> => {
+  const query = firestore.collection(graphCollectionName).doc(id);
+  const doc = await query.get();
+  return Promise.resolve(doc);
+};
+
+export const getProject = async (id: string): Promise<{ project: Project, doc: any }> => {
+  const doc = await getProjectDocument(id);
+  if (doc.exists) {
+    const id = doc.ref.id;
+    const json = doc.data();
+    const project = new Project();
+    project.fromJSON(id, json as ProjectJSONType);
+    return Promise.resolve({
+      project, doc: json
+    });
+  }
+  return Promise.reject(new Error('this file is not existed.'));
+};
+
+export const duplicateProject = async (id: string): Promise<Project> => {
+  const { project } = await getProject(id);
+  project.title += ' copy';
+  project.timestamp = firebase.firestore.Timestamp.now();
+  const ref = await firestore.collection(graphCollectionName).add(project.toJSON());
+  project.id = ref.id;
+  return Promise.resolve(project);
+};
+
+export const deleteProject = async (id: string): Promise<void> => {
+  const doc = await getProjectDocument(id);
+  if (doc.exists) {
+    const data = doc.data() as ProjectJSONType;
+    data.deleted = true;
+    await doc.ref.update(data);
+    return Promise.resolve();
+  }
+  return Promise.reject(new Error('this file is not existed.'));
+};
+
+export const getTimestamp = (): firebase.firestore.Timestamp => {
+  return firebase.firestore.Timestamp.now();
+};
