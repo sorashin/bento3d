223002997afbaf79c7958f1e4ad89503d88e63ab
sorashin <naka52shin@gmail.com>
2024-03-22 02:51:53 +0900

    Revert "integrate GPT"
    This reverts commit 73f57efc36fe663134c511f6268d9fbbfcb95687.

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -2,13 +2,41 @@
 
 import * as user from './user';
 import { atom } from 'jotai'
+export type BoxConfig = {
+    totalWidth:number,
+	totalHeight:number,
+	totalDimension:number,
+	offset:number,
+	colorMode:number,
+	partitionThickness:number
+}
 
+export const boxConfigAtom = atom<BoxConfig>({
+    totalWidth: 100,
+    totalHeight: 100,
+    totalDimension: 100,
+    offset: 3,
+    colorMode: 0,
+    partitionThickness: 1
+})
 
+export type Grid = {
+    index:number,
+	label:string,
+	width:number,
+	height:number,
+	grid:number,
+}
 
-
-// export const getters = getterTree(state, {});
-// export const mutations = mutationTree(state, {});
-// export const actions = actionTree({ state, getters, mutations }, {});
+export const gridAtoms = atom<Grid[]>([
+    {
+        index: 0,
+        label: '',
+        width: 100,
+        height: 100,
+        grid: 1
+    }
+])
 
 export const openAIAPIKeyAtom = atom<string>('')
 export const selectedColorAtom = atom<string>('')
